29f6e06571d88b3080ee904a70a7af0cb946969c
kashifnaseem50 <kashifnaseem506@yahoo.com>
2022-09-12 20:22:50 +0500

    WIP

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hi There')
+  });
+
+
+  // app.get('/get', (req, res) => {
+  //   const SelectQuery = " SELECT * FROM books_reviews";
+  //   db.query(SelectQuery, (err, result) => {
+  //     res.send(result)
+  //   })
+  // })
+
+  // app.post("/insert", (req, res) => {
+  //   const bookName = req.body.setBookName;
+  //   const bookReview = req.body.setReview;
+  //   const InsertQuery = "INSERT INTO books_reviews (book_name, book_review) VALUES (?, ?)";
+  //   db.query(InsertQuery, [bookName, bookReview], (err, result) => {
+  //     console.log(result)
+  //   })
+  // })
+
+  app.listen('3001', () => { })
